b90ae91f1a38ec019f6b72b27afcd2f649d27e5b
Marcos <mpfaraujo@gmail.com>
2023-06-17 20:23:42 -0300

    Imagens de logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Pesquisa } from '@/components/pesquisa'
 export default async function Home() {
   return (
     <><div className='flex justify-center w-full'>
-    <img src="http://www.cefet-rj.br/attachments/article/431/horiz_azul.png"  width="300px"/><br/>
+    <img src="/horiz_azul"  width="300px"/><br/>
     </div>
     <Pesquisa />
     </>
